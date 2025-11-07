4096d7745c64a5a5933b5f6caaf385d5d0441e98
RodrigoPeterson <rodrigopetersonfreelas@gmail.com>
2025-11-07 17:10:19 -0300

    Atividades de python para concluir

---
+++ Atividades-de-python/07_atividade_python/classes.py
@@ -0,0 +1,8 @@
+# biblioteca
+import os
+
+# classes
+class Titular_da_conta:
+    # construtor
+    def __init__(self, nome_do_titular, cpf, numero_da_agencia, numero_da_conta, saldo):
+        pass
---
+++ Atividades-de-python/07_atividade_python/main.py
@@ -0,0 +1,16 @@
+# TODO: atividade 07
+"""
+Crie um app de banco. O programa deverá ter a classe Conta, com os atributos:
+4 - Titular da conta (nome)
+5 - CPF do titular
+6 - Número da agência
+7 - Número da conta
+8 - Saldo
+9 O usuário irá inserir os dados "Titular da conta" e "CPF", e poderá escolher entre as seguintes opções:
+- Consultar dados da conta
+- Depositar valor
+- Sacar valor
+- Sair do programa
+"""
+
+
